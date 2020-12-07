980b39721f2cb0848e9de8e8df4287f2650a17db
raferalston <like2pwnyou@yandex.ru>
2020-12-07 15:45:48 +0300

    alpha version

--- avatar_alpha.py
+++ avatar_alpha.py
@@ -2,10 +2,12 @@ import requests
 
 from bs4 import BeautifulSoup
 
+query = 'котики'
 url = f'https://www.kiddle.co/s.php?q={query}'
 
 page = requests.get(url).text
 soup = BeautifulSoup(page, 'html.parser')
+
 
 for raw_img in soup.find_all('img'):
     link = raw_img.get('src')
@@ -14,3 +16,4 @@ for raw_img in soup.find_all('img'):
         with open("./today_avatar.jpg", 'wb') as f:
             f.write(response.content)
         break
+
